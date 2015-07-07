32a4769a0454390ff8c35b2ba97f27fe365b6336
Daniel Hunt <daniel@brokertech.co.uk>
2015-07-07 20:43:09 +0100

    Add create payments table migration

---
+++ database/migrations/2015_07_07_193524_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->char('description');
+            $table->decimal('amount');
+
+            $table->integer('user_id');
+            $table->string('source_type');
+            $table->integer('source_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('payments');
+    }
+}
