59f26289b05b3698699623aa80c2f5088e545c29
Daniel Hunt <daniel@brokertech.co.uk>
2015-09-05 15:22:52 +0100

    Add colour options to model

--- app/Envelope.php
+++ app/Envelope.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Envelope extends Model {
+class Envelope extends Model
+{
+
+    const BLUE = 0;
+    const GREEN = 1;
+    const YELLOW = 2;
+    const ORANGE = 3;
+    const PINK = 4;
+    const PURPLE = 5;
+
+    public static $colours = [
+        self::BLUE => 'blue',
+        self::GREEN => 'green',
+        self::YELLOW => 'yellow',
+        self::ORANGE => 'orange',
+        self::PINK => 'pink',
+        self::PURPLE => 'purple',
+    ];
 
     /**
      * The database table used by the model.
@@ -16,7 +33,7 @@ class Envelope extends Model {
      *
      * @var array
      */
-    protected $fillable = ['name', 'amount'];
+    protected $fillable = ['name', 'amount', 'colour'];
 
     public function user()
     {
@@ -42,6 +59,34 @@ class Envelope extends Model {
     public function destination()
     {
         return $this->morphMany('App\Transaction', 'destination');
+    }
+
+    public function getAmountAttribute($value)
+    {
+        setlocale(LC_MONETARY, $this->user->locale);
+        $money = money_format('%.2n', $value / 100);
+        setlocale(LC_MONETARY, env('LOCALE'));
+        return $money;
+    }
+
+    public function getColourAttribute($value)
+    {
+        return array_get(self::$colours, $value);
+    }
+
+    public function setColourAttribute($value)
+    {
+        return $this->attributes['colour'] = array_search($value, self::$colours);
+    }
+
+    public function colourOptions()
+    {
+        $colours = self::$colours;
+        $colour = $this->colour;
+        if (($key = array_search($colour, $colours)) !== false) {
+            unset($colours[$key]);
+        }
+        return $colours;
     }
 
 }
