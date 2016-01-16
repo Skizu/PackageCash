abc15f3acba0d8e0a60c78dec2b55b29825b879f
Daniel Hunt <daniel@brokertech.co.uk>
2016-01-16 22:19:28 +0000

    Remove user created event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,9 +13,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\UserWasCreated' => [
-            'App\Listeners\AddToAudit',
-        ],
         'App\Events\ChequeWasCreated' => [
             'App\Listeners\AddToAudit',
         ],
