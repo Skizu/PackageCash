8614e9a9803006e1522a825b42eaa4e530804a15
Daniel Hunt <daniel@brokertech.co.uk>
2016-01-17 13:17:27 +0000

    Updated transaction audit message

--- resources/views/audit/transaction-created.blade.php
+++ resources/views/audit/transaction-created.blade.php
@@ -1,3 +1,4 @@
 <a href="{{ route('profile.show', [$UserID]) }}">{{ $users->find($UserID)->name }}</a> withdrew
-{{ $Money::create($Data->amount)->formatMoney() }} for the following reason(s):
+{{ $Money::create($Data->amount)->formatMoney() }} from <a
+        href="{{ route('envelope.show', [$Data->envelope->id]) }}">{{ $Data->envelope->name }}</a> for the following reason(s):
 <div>{{ $Data->description }}</div>
