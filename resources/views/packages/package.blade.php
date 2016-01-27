a215b6e316b27ff06679e145d582c1a202b90ff9
Daniel Hunt <daniel@brokertech.co.uk>
2016-01-27 20:33:40 +0000

    Update package views

---
+++ resources/views/packages/package.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-xs-12 col-md-3">
+        <div class="action-panel">
+            <h5 class="header">Actions</h5>
+            <a class="col-md-12 super-action">
+                <span>Edit</span><span class="info">Package details</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-4">
+        <h5 class="header">Envelopes</h5>
+        {{ dump($package->envelopes) }}
+    </div>
+    <div class="col-xs-12 col-md-5">
+        <h5 class="header">Transaction History</h5>
+        @include('audit-log.logs', ['log' => $package->getAuditLog()])
+    </div>
+</div>
