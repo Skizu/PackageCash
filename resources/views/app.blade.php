b4ff1cd5ef92c994e2896c7f5a5a469ffac2ff6d
Daniel Hunt <daniel@brokertech.co.uk>
2016-01-28 10:33:51 +0000

    Update default font weight

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
-    <link href="//fonts.googleapis.com/css?family=Lato:100,300,400,500,700" rel="stylesheet" type="text/css">
+    <link href="//fonts.googleapis.com/css?family=Lato:300,400,500,700" rel="stylesheet" type="text/css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"
           type="text/css">
 
